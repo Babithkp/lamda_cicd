b8a1fea3099f16d18c23605bee52e9ae0a75e40b
Babithkp <babithkullachetti44@gmail.com>
2024-12-30 21:57:10 +0530

    new commit

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
 app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
