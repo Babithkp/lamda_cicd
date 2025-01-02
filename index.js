cfcc500b016782db8fbd26308255e63ac7974164
Babithkp <babithkullachetti44@gmail.com>
2025-01-02 09:30:54 +0530

    new router has been added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import serverless from "serverless-http";
 import cors from "cors";
+import axios from "axios";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -12,5 +14,12 @@ app.get("/", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("Hello World!");
 });
+
+app.get("/getUser",async (req,res)=>{
+  const user = await axios.get("https://jsonplaceholder.typicode.com/users");
+  console.log(user.data);
+  res.send(user.data);
+})
+
 
 export const handler = serverless(app);
